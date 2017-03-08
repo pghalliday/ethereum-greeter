3c1b3a7d03dd359c58f6bef9df4ea5d05d6d87de
Peter Halliday <pghalliday@gmail.com>
2017-03-08 16:19:27 +0100

    report contract gas usage

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf $DIR/build
 mkdir -p $DIR/build
 cd $DIR
-solc --bin --abi --optimize -o $DIR/build greeter.sol
+solc --gas --bin --abi --optimize -o $DIR/build greeter.sol
